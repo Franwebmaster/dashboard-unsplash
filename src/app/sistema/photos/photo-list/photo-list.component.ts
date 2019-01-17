ce470e16644bc05b2cd8613af810f51a6e9fe273
Francisco <franciscobarros754@gmail.com>
2019-01-17 02:26:22 -0200

    criando lista de fotos

--- src/app/sistema/photos/photo-list/photo-list.component.ts
+++ src/app/sistema/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from './../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
@@ -9,7 +10,7 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: Object[] = [];
+  photos: Photo[] = [];
 
   constructor(private photoServive: PhotoService) {}
   
